c1cabf255dd21eee555da5d627305aea45d80261
Yazmeen Vazquez <yazmeen.vazquez14@gmail.com>
2017-01-19 23:43:29 +0000

    Drawing

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 TinyTurtle.apply(window); 
 
-forward();
+right(45);
+forward(25);
+right(90);
+forward(25);
+right(135);
+forward(35);
+left(90);
+forward(60);
+left(90);
+forward(35);
+left(90);
+forward(60);
+right(90);
+penUp(); /*Function that helps move w/o drawing*/
+forward(50);
+penDown(); /*Goes back to normal*/
+forward(7);
+left(7);
+forward(7);
+left(7);
+forward(7);
+forward(7);
+left(7);
+forward(7);
+left(7);
+forward(7);
+left(7);
+forward(7);
+left(7);
+forward(7);
+left(135);
+
+stamp();
